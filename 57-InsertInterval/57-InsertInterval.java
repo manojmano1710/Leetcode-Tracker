bf5c8d1d6f668c41324c4c806496ec70ea550fe4
Manoj k <manojmano9791537730@gmail.com>
2026-07-09 15:10:50 +0530

    Create 57-InsertInterval

---
+++ 57-InsertInterval/57-InsertInterval.java
@@ -0,0 +1,31 @@
+// Last updated: 09/07/2026, 15:10:50
+class Solution {
+    public int[][] insert(int[][] intervals, int[] newInterval) {
+
+        List<int[]> result = new ArrayList<>();
+        int i = 0;
+
+        // Step 1: Add all intervals ending before newInterval starts
+        while (i < intervals.length && intervals[i][1] < newInterval[0]) {
+            result.add(intervals[i]);
+            i++;
+        }
+
+        // Step 2: Merge overlapping intervals
+        while (i < intervals.length && intervals[i][0] <= newInterval[1]) {
+            newInterval[0] = Math.min(newInterval[0], intervals[i][0]);
+            newInterval[1] = Math.max(newInterval[1], intervals[i][1]);
+            i++;
+        }
+
+        result.add(newInterval);
+
+        // Step 3: Add remaining intervals
+        while (i < intervals.length) {
+            result.add(intervals[i]);
+            i++;
+        }
+
+        return result.toArray(new int[result.size()][]);
+    }
+}
