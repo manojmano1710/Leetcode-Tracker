ce9b3dea4aa767129739451c26c02617da4e72f5
Manoj k <manojmano9791537730@gmail.com>
2026-07-09 15:08:26 +0530

    Create 109-ConvertSortedListToBinarySearchTree

---
+++ 109-ConvertSortedListToBinarySearchTree/109-ConvertSortedListToBinarySearchTree.java
@@ -0,0 +1,23 @@
+// Last updated: 09/07/2026, 15:08:25
+class Solution {
+    public TreeNode sortedListToBST(ListNode head) {
+        if (head == null) return null;
+        if (head.next == null) return new TreeNode(head.val);
+
+        ListNode slow = head, fast = head, prev = null;
+
+        while (fast != null && fast.next != null) {
+            prev = slow;
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        if (prev != null) prev.next = null;
+
+        TreeNode root = new TreeNode(slow.val);
+        root.left = sortedListToBST(head);
+        root.right = sortedListToBST(slow.next);
+
+        return root;
+    }
+}
